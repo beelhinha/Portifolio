c9d83abb9253774982bc92b063b7a8a1c6311bb5
beelhinha <156480929+beelhinha@users.noreply.github.com>
2024-08-17 19:02:01 -0300

    Delete site directory

--- site/index.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const email = document.getElementById('email').value;
-    if (email.includes('@')) {
-        alert('O contato do email foi aceito.');
-    } else {
-        alert('O contato do email não foi aceito.');
-    }
-});
