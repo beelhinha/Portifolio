af3796046c171a852d617b6b519d4d631e5965fe
beelhinha <156480929+beelhinha@users.noreply.github.com>
2024-08-17 19:02:30 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const email = document.getElementById('email').value;
+    if (email.includes('@')) {
+        alert('O contato do email foi aceito.');
+    } else {
+        alert('O contato do email não foi aceito.');
+    }
+});
